30e2efc14d2ddba8ae1ccb06eccf187276b35a9c
xiaoma <xiaoma@xiaomas-MacBook-Air-2.local>
2013-04-06 16:24:30 -0700

    add logic to get names, partially implemented

--- phd4.js
+++ phd4.js
@@ -1,15 +1,24 @@
+Players = new Meteor.Collection("players");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
+  Template.head.greeting = function () {
     return "Welcome to phd4.";
   };
 
-  Template.hello.events({
-    'click input' : function () {
+  Template.head.events({
+    'keyup input.username' : function (e) {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+        Session.set("name") = $('input.username').val();
+        console.log("You are: "+ name);
     }
   });
+
+  Template.head.current_player = function() {
+    var name = Session.get("name");
+    if (name !== undefined) return name;
+    else return 'new player';
+  };
 }
 
 if (Meteor.isServer) {
