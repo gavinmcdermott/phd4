66c09b885010915fbaad6155de21197557426cf3
logicmason <wei.xiaoma@gmail.com>
2013-04-06 17:15:56 -0700

    adds players collection

--- phd4.js
+++ phd4.js
@@ -10,16 +10,18 @@ if (Meteor.isClient) {
       // template data, if any, is available in 'this'
       if (e.keyCode === 13) {
         e.preventDefault();
-        player = $('input.username').val();
+        var playerName = $('input.username').val();
+        var player = {name: playerName, score: 0};
+        Players.insert(player);
         Session.set("current_player", player);
-        console.log("You are: "+ player);
+        console.log("You are: "+ player.name);
       }
     }
   });
 
   Template.head.current_player = function() {
-    var name = Session.get("current_player");
-    if (name) return name;
+    var player = Session.get("current_player");
+    if (player) return player.name;
     else return false;
   };
 }
