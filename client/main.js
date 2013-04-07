c3361f1efc0448a25caf0e3027fae62235049d0d
logicmason <wei.xiaoma@gmail.com>
2013-04-07 00:44:23 -0700

    wheeee!

--- client/main.js
+++ client/main.js
@@ -15,6 +15,11 @@ Template.head.events({
     Players.remove(player._id);
     amplify.store("current_player", false);
     Session.set("current_player", false);
+  },
+  'click h1' : function() {
+    console.log("yo");
+    Meteor.call("serveTime");
+    console.log(Session.get("time"));
   }
 });
 
@@ -48,6 +53,14 @@ Meteor.startup(function() {
     if (logged_in) {
       console.log(logged_in);
       console.log("Sheeeeiit");
+    }
+  });
+
+  Meteor.methods({
+    serveTime: function() {
+      var time = Date();
+      console.log("serving time");
+      Session.set("time", time);
     }
   });
 });
