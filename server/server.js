47dcc7e33da58d20aeb2648e71499f01f1d6c862
logicmason <wei.xiaoma@gmail.com>
2013-04-07 06:46:02 -0700

    adds currentPhoto to server-side

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 Meteor.startup(function () {
-
+  var currentPhoto = null;
   // Photos.remove({});
   // Guesses.remove({});
 
   Meteor.methods({
+
+    getCurrentPhoto: function() {
+      console.log(currentPhoto);
+      if (currentPhoto) return currentPhoto;
+    },
 
     getRandPhoto: function() {
       var results = Photos.find({}).fetch();
       var range = _.size(results);
       var rand = Math.floor(Math.random() * range);
       var photo = results[rand];
+      currentPhoto = photo;
       return photo;
     },
 
