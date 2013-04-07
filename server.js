25e0d491712aa20162103761dc047b9c4c390b67
MrBri <ty8bri@gmail.com>
2013-04-06 23:02:12 -0700

    500px

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ if (Meteor.isServer) {
     // _500px.api('/photos', { feature: 'popular', page: 1 }, function (response) {
     //   console.log(response.data.photos);
     // });
+
+    _500px.init({
+      sdk_key: '0a722b0030e8d7d4a5cc8f6f8f916d67ba9c38e8'
+    });
+
+    _500px.api('/photos/search', { term: 'funny', rpp: 100, page: 1, image_size: 3 }, function (response) {
+      for (var i = 0; i < 5; i++) {
+        var ran = Math.floor(Math.random() * 101);
+        console.log(ran);
+        var url = (response.data.photos[ran].image_url);
+        $('body').append('<img src="'+url+'" />');
+      };
+    });
+
+
   });
 
   // Meteor.methods({
