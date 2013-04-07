815b2d62cb9a54188e5dcfc782871a2addfe0749
logicmason <wei.xiaoma@gmail.com>
2013-04-06 22:07:29 -0700

    rips out fb, accounts ui, etc, uses amplify.store

--- client/main.js
+++ client/main.js
@@ -10,15 +10,16 @@ Template.head.events({
       var playerName = $('input.username').val();
       var player = {name: playerName, score: 0};
       Players.insert(player);
-      localStorage.setItem("current_player", player._id);
-      console.log("Your name: "+ player.name + " your id: " + player._id);
+      amplify.store("current_player", player);
+      // State.findOne("signed_in")
+      console.log("You are: "+ player.name);
     }
   }
 });
 
 Template.head.current_player = function() {
-  var player = localStorage.getItem("current_player");
-  if (player && player != "undefined") return Players.find(player).fetch()[0].name;
+  var player = amplify.store("current_player");
+  if (player) return player.name;
   else return false;
 };
 
@@ -29,21 +30,12 @@ Template.sidebar.players = function() {
 
 
 Meteor.startup(function() {
-  // _500px.init({
-  //   sdk_key: '8f9e1716ef34c4b9f771cef66ebaee3de2ce99e0'
-  // });
-
-  // _500px.api('/photos/search', function (response) {
-  //   if (response.success) {
-  //     console.log('There are ' + response.data.photos.length + ' photos.');
-  //   } else {
-  //     console.log('Unable to complete request: ' + response.status + ' - ' + response.error_message);
+  // var logged_in = amplify.store();
+  // Meteor.autorun(function() {
+  //   if (logged_in) {
+  //     console.log(logged_in);
+  //     console.log("Sheeeeiit");
   //   }
   // });
 
-  // _500px.getAuthorizationStatus(function (status) {
-  //   if (status == 'not_logged_in' || status == 'not_authorized') {
-  //     _500px.login();
-  //   }
-  // });
 });
