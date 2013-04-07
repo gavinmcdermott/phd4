be7ebb3bec1ec34a61ca2d65ced82537aeeb4c46
gavinmcdermott <gavinmcdermott@gmail.com>
2013-04-07 04:21:41 -0700

    add photo storage with random variable for selection

--- client/main.js
+++ client/main.js
@@ -1,9 +1,26 @@
+Template.head.current_player = function() {
+  var player = Session.get("current_player");
+  if (player) return player.name;
+  else return false;
+};
+
+Template.head.adding_emotion_to_image = function() {
+  return Session.get('adding_emotion_to_image');
+};
+
+Template.head.guessing_answer = function() {
+  return Session.get('guessing_answer');
+};
+
 Template.head.events({
   'keydown input.username' : function (e) {
     if (e.keyCode === 13) {
       e.preventDefault();
       var playerName = $('input.username').val();
-      var player = {name: playerName, score: 0};
+      var player = {
+        name: playerName,
+        score: 0
+      };
       Players.insert(player);
       Session.set("current_player", player);
       amplify.store("current_player", player);
@@ -18,14 +35,16 @@ Template.head.events({
   }
 });
 
-Template.head.current_player = function() {
-  var player = Session.get("current_player");
-  if (player) return player.name;
-  else return false;
-};
-
 Template.sidebar.players = function() {
   return Players && Players.find({}).fetch();
+};
+
+Template.add_emotion_form.selected_photo = function() {
+  var rand = Math.random();
+  var result = Photos.find( { random : { $gte : rand } } ).fetch()[0];
+  if ( result === null ) {
+    result = Photos.find( { random : { $lte : rand } } ).fetch()[0];
+  }
 };
 
 Meteor.startup(function() {
@@ -33,14 +52,16 @@ Meteor.startup(function() {
   _500px.init({
     sdk_key: '0a722b0030e8d7d4a5cc8f6f8f916d67ba9c38e8'
   });
-
   _500px.api('/photos/search', { term: 'funny', rpp: 100, page: 1, image_size: 3 }, function (response) {
     for (var i = 0; i < 9; i++) {
       var ran = Math.floor(Math.random() * 101);
       var url = (response.data.photos[ran].image_url);
-      // $('body').append('<img src="'+url+'" />');
       if (!Photos.findOne({url: url})) {
-        Photos.insert({url: url});
+        Photos.insert({
+          url: url,
+          random : Math.random(),
+          emotion: null
+        });
       }
     }
   });
@@ -49,8 +70,11 @@ Meteor.startup(function() {
     var logged_in = amplify.store("current_player");
     Session.set("current_player", logged_in);
     if (logged_in) {
-      console.log(logged_in);
-      console.log("Sheeeeiit");
+      amplify.store("guessing_answer", true);
+      Session.set("guessing_answer", true);
+
+      amplify.store("adding_emotion_to_image", true);
+      Session.set("adding_emotion_to_image", true);
     }
   });
 });
